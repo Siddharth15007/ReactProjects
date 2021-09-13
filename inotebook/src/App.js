36784fc56a7b48023a30a7d3f496a96678fee1fc
Siddharth Parekh <siddharthparekh40@gmail.com>
2021-09-14 01:15:40 +0530

    Create Navbar and Router in React frontend

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import { Home } from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ inotebook/src/components/About.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const About = () => {
+  return <div>this is bhosdina</div>;
+};
+export default About;
