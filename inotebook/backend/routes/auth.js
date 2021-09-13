719eca1ae3a031db9e92e15eb0a11ad2d18a7db4
Siddharth Parekh <siddharthparekh40@gmail.com>
2021-09-13 21:50:08 +0530

    Add JWT auth

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,10 +2,13 @@ const express = require("express");
 const User = require("../models/Users");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "SIDDHARTH";
 
 //Create a User using: POST "/api/auth/", No login required
 router.post(
-  "/",
+  "/createuser",
   [
     body("email").isEmail(),
     body("name").isLength({ min: 3 }),
@@ -26,13 +29,25 @@ router.post(
           .status(400)
           .json({ error: "Sorry! User with this email already exists." });
       }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
       user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
       });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
 
-      res.json(user);
+      res.json({ authToken });
+
+      //res.json(user);
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some error ocurred!");
