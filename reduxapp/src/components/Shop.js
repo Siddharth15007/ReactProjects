8ca90e3dbebd3247fce7bbb21c6908bdd647817e
Devang139 <siddharthparekh40@gmail.com>
2021-09-16 14:28:54 +0530

    Redux App Setup Complete

---
+++ reduxapp/src/components/Shop.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div>
+      <button className="btn btn-primary">+</button>
+      Buy this Item
+      <button className="btn btn-primary">-</button>
+    </div>
+  );
+};
+
+export default Shop;
